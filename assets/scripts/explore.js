a16de4504d81724c42a3d54fe4867d52c7b3ccbd
Ettore Hidoux <ettore.hidoux@imt-atlantique.net>
2022-10-24 19:05:42 -0700

    to fix smiling face and confetti

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,56 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const synth = window.speechSynthesis;
+
+  const text = document.querySelector('textarea');
+  const inputTxt = document.querySelector('.txt');
+  const voiceSelect = document.getElementById('voice-select');
+  const img = document.querySelector('img[alt="Smiling face"]')
+  const start = document.querySelector('button')
+
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length ; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  start.addEventListener('click', () => {
+  
+    const utterThis = new SpeechSynthesisUtterance(text.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for (let i = 0; i < voices.length ; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+
+    // utterThis.pitch = pitch.value;
+    // utterThis.rate = rate.value;
+
+    img.src = 'assets/images/smiling-open.png';
+    synth.speak(utterThis);
+    inputTxt.blur();
+  })
+
+  await (img.src = 'assets/images/smiling.png');
+
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,58 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+import { JSConfetti } from './js-confetti.browser.js';
+
 function init() {
-  // TODO
+  const select1 = document.getElementById('horn-select');
+  const img1 = document.querySelector('img[alt="No image selected"]');
+  const audio = document.querySelector('audio[class="hidden"]')
+
+  const select2 = document.getElementById('volume');
+  const img2 = document.querySelector('img[alt="Volume level 2"');
+
+  const select3 = document.querySelector('button');
+  const jsConfetti = new JSConfetti();
+
+  select1.addEventListener('change', (event) => {
+    if (event.target.value == 'air-horn') {
+      img1.src='assets/images/air-horn.svg';
+      audio.src='assets/audio/air-horn.mp3';
+    }
+    if (event.target.value == 'car-horn') {
+      img1.src='assets/images/car-horn.svg';
+      audio.src='assets/audio/car-horn.mp3';
+    }
+    if (event.target.value == 'party-horn') {
+      img1.src='assets/images/party-horn.svg';
+      audio.src='assets/audio/party-horn.mp3';
+    }
+  });
+
+  select2.addEventListener('change', (event) => {
+    if (event.target.value == 0) {
+      img2.src='assets/icons/volume-level-0.svg';
+      audio.volume = event.target.value/100;
+    }
+    else if (event.target.value < 33) {
+      img2.src='assets/icons/volume-level-1.svg';
+      audio.volume = event.target.value/100;
+    }
+    else if (event.target.value < 67) {
+      img2.src='assets/icons/volume-level-2.svg';
+      audio.volume = event.target.value/100;
+    }
+    else {
+      img2.src='assets/icons/volume-level-3.svg';
+      audio.volume = event.target.value/100;
+    }
+  });
+  
+  select3.addEventListener('click', () => {
+    audio.play();
+    if (select1.target.value == 'party-horn') {
+      // TO DO
+      jsConfetti.addConfetti()
+    };
+  });
 }
